e7501d2171f5d2c1beb591bfc8ee5050c35b22c4
Esteban <teby@hotmail.com.ar>
2023-06-29 19:12:38 -0300

    juego ahorcado entrega2

--- JS/app.js
+++ JS/app.js
@@ -1,17 +1,82 @@
-let golesMessi = 806; 
+// Palabras disponibles para adivinar
+const palabras = ["manzana", "pera", "naranja", "banana", "uva"];
 
-function adivinarGoles() {
-  let golesAdivinados = parseInt(prompt("Adivina cuántos goles tiene Messi:"));
+// Palabra aleatoria seleccionada para adivinar
+let palabraSeleccionada = "";
 
-  if (golesAdivinados === golesMessi) {
-    alert("¡Correcto! Messi the GOAT ha marcado " + golesMessi + " goles.");
-  } else if (golesAdivinados < golesMessi) {
-    alert("MAS MAS ...ES MESSI. Inténtalo nuevamente.");
-    adivinarGoles(); 
-  } else {
-    alert("Te pasaste todavia no llego a ese. Inténtalo nuevamente.");
-    adivinarGoles(); 
-  }
+// Palabra mostrada al jugador con guiones bajos
+let palabraMostrada = "";
+
+// Intentos restantes
+let intentosRestantes = 6;
+
+// Función para seleccionar una palabra aleatoria del array de palabras
+function seleccionarPalabra() {
+  const indiceAleatorio = Math.floor(Math.random() * palabras.length);
+  palabraSeleccionada = palabras[indiceAleatorio];
+  palabraMostrada = "_".repeat(palabraSeleccionada.length);
 }
 
-adivinarGoles();
+// Función para verificar si una letra ingresada por el jugador está en la palabra seleccionada
+function verificarLetra(letra) {
+  if (palabraSeleccionada.includes(letra)) {
+    for (let i = 0; i < palabraSeleccionada.length; i++) {
+      if (palabraSeleccionada[i] === letra) {
+        const nuevaPalabraMostrada =
+          palabraMostrada.substring(0, i) +
+          letra +
+          palabraMostrada.substring(i + 1);
+        palabraMostrada = nuevaPalabraMostrada;
+      }
+    }
+
+    if (!palabraMostrada.includes("_")) {
+      alert("¡Felicitaciones! Has adivinado la palabra.");
+      alert(`La palabra era: ${palabraSeleccionada}`);
+      return true;
+    }
+  } else {
+    intentosRestantes--;
+    alert(`Letra incorrecta. Te quedan ${intentosRestantes} intentos.`);
+
+    if (intentosRestantes === 0) {
+      alert("¡Oh no! Has perdido.");
+      alert(`La palabra era: ${palabraSeleccionada}`);
+      return true;
+    }
+  }
+
+  return false;
+}
+
+// Función para reiniciar el juego
+function reiniciarJuego() {
+  seleccionarPalabra();
+  intentosRestantes = 6;
+}
+
+// Iniciar el juego
+seleccionarPalabra();
+
+// Variable para almacenar si el juego ha finalizado
+let juegoFinalizado = false;
+
+// Loop principal del juego
+while (!juegoFinalizado) {
+  const letraIngresada = prompt(
+    `Palabra: ${palabraMostrada}\nIntentos restantes: ${intentosRestantes}\nIngresa una letra:`
+  );
+
+  if (letraIngresada === null) {
+    break;
+  }
+
+  if (letraIngresada.length !== 1 || !/^[a-zA-Z]+$/.test(letraIngresada)) {
+    alert("Ingresa una letra válida.");
+    continue;
+  }
+
+  juegoFinalizado = verificarLetra(letraIngresada.toLowerCase());
+}
+
+
